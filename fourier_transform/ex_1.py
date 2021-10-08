80c922361f9510e01e37541c1b57e9d1e4fb0df3
MaxiHartmann <maxi.hartmann207@gmail.com>
2021-10-08 18:57:34 +0200

    parameter amplitude is controlling the volume

--- fourier_transform/ex_1.py
+++ fourier_transform/ex_1.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 
 SAMPLE_RATE = 44100     # Hertz
 DURATION = 5            # Seconds
+AMPLITUDE = 0.01
 
 def generate_sine_wave(freq, sample_rate, duration):
     x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
@@ -10,13 +11,17 @@ def generate_sine_wave(freq, sample_rate, duration):
     y = np.sin((2 * np.pi) * frequencies)
     return x, y
 
+base_note=440
 _, nice_tone = generate_sine_wave(440, SAMPLE_RATE, DURATION)
-_, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
-noise_tone = noise_tone * 0.0
+mixed_tone = nice_tone
 
-mixed_tone = nice_tone + noise_tone
+for i in range(1,9):
+    _, noise_tone = generate_sine_wave(base_note*i, SAMPLE_RATE, DURATION)
+    noise_tone = noise_tone / i
+    mixed_tone += noise_tone
 
-normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767 * 0.7)
+normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
+normalized_tone = normalized_tone/max(normalized_tone) * AMPLITUDE
 
 plt.plot(normalized_tone[:1000])
 plt.show()
